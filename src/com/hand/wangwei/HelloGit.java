d5bd74e344a6a9d33b46ed53316cd11fe915d00b
22253 <wei.wang23@hand-china.com>
2019-04-09 15:15:59 +0800

    ww

--- src/com/hand/wangwei/HelloGit.java
+++ src/com/hand/wangwei/HelloGit.java
@@ -5,6 +5,6 @@ package com.hand.wangwei;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.err.println("hello left_ww");
-		System.err.println("我是hot_fix");
+		System.err.println("我是hot_fixoo");
 	}
 }
